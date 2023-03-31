96743368dc99132472e49449c064bc14315d2a75
Wendina <wendina.teng@gmail.com>
2023-03-31 18:29:35 +0000

    Remove unnecessary comments to improve readability.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,44 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-// function prevents XSS with escaping, what it does is <script> would be converted to &lt;script&gt;. 
-// The HTML tag would then be visible to the user, but not evaluated as a tag by the browser.
-// It takes in a string argument and returns the escaped HTML equivalent of the string.
-const esc = function (str) {
+// prevents XSS with escaping
+const esc = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
-// function creates HTML markup using template literals and returns it to the caller
+// Creates HTML markup using template literals and returns the article element to the caller
 const createTweetElement = function(data) {
 
   // Use the timeago.format method to display the time passed since a tweet was created
@@ -68,72 +38,57 @@ const createTweetElement = function(data) {
     </article>
   `);
 
-  return $tweet; // returns the article element
+  return $tweet;
 };
 
-// function takes in an array of objects, calls the createTweetElement which returns the article element and render them to DOM
+// Calls the createTweetElement which returns the article element and appends it to the tweets-container
 const renderTweets = function(tweets) {
-  // loops through tweets
   tweets.forEach(userInfo => {
-
-    // calls createTweetElement for each tweet, takes return value and appends it to the tweets-container
     const $returnedTweet = createTweetElement(userInfo);
     $('#tweets-container').prepend($returnedTweet);
   });
 };
 
-// this function uses jQuery to make a request to /tweets/ and receive the array of tweets as JSON
-// the success callback function calls up renderTweets function and passes it the response from the AJAX request
+// Make a request to /tweets/ to fetch the data from server
 const loadTweets = function() {
-  // ajax method to fetch(GET) data from the server
   $.ajax({
     method: "GET",
     url: "/tweets/",
-    // dataType: "json", DO I NEED THIS?? *******************
   })
     .then(function(data) {
       renderTweets(data);
     });
 };
 
-// this function pulls the last item of the array from /tweets/ and creates the article element by calling crateTweetElement function
-// and then takes the returned value and appends it to the #tweets-container to the top
-// this is so that we don't have to keep refreshing thr browser to see the new tweet added
+// Make a request to /tweets/ and pulls the last item of the array from the data received and show it in the feed right away
+// this is so that we don't have to keep refreshing the browser to see the new tweet added
 const addNewTweet = function() {
   $.ajax({
     method: "GET",
     url: "/tweets/",
   })
     .then(function(data) {
-      // Get the last item of the array (because when a tweet is submitted, the tweet gets added inside the array as the last item)
       const index = data.length - 1;
-      // Grab the newly created tweet and add it on top of the tweet-container
       $("#tweets-container").prepend(createTweetElement(data[index]));
     });
 };
 
 $(document).ready(function() {
-
-  // call the loadTweets function so it shows the tweets that were previously submitted
   loadTweets();
 
-  // Use jQuery library to add event listener for "submit"
+  // Event handler on form submission
   $('.form').on('submit', function(event) {
-
-    // prevent the default form submission behaviour
     event.preventDefault();
 
-    // grab the input from the text-area and serialize it
-    // serialize the data coming from the form and send it to the server as a query string
     const input = $("#tweet-text").serialize();
 
-    //if statements to handle empty strings or over character limit for the textarea
+    // Data checks and validations
     const tweetLength = $('#tweet-text').val().length;
 
     if (tweetLength) {
       $('.error-messages').slideUp();
     }
-    
+
     if (tweetLength > 140) {
       $('.error-messages').text(`Character limit exceeded. Please try again.`).slideDown();
       return;
@@ -144,16 +99,15 @@ $(document).ready(function() {
       return;
     }
 
-    // ajax method is used to send the POST request to the server
+    // Make the POST request to the server
     $.ajax({
       method: "POST",
       url: "/tweets/",
       data: input,
     })
+      // after submitting the form, clear the text that was written and reset the counter
       .then(function() {
-        // clear what was written
         $('#tweet-text').val("");
-        // counter resets
         $('.counter').val(140);
         addNewTweet();
       });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,17 @@
-// runs a callback when the DOM is ready to be manipulated with jQuery. 
+// Run a callback when the DOM is ready to be manipulated with jQuery
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  // register an event handler to the textarea element for the form inside of the .new-tweet section.
-  // we want to listen for any input from the user and count down the number as they input more characters
-  $('#tweet-text').on('input', function(event){
-
-    // $(this) refers to the current '<textarea>' element that triggered the event
-    // grab the value of the textarea and find the length of it, then assign it to a variable
+  // Event handler (for the textarea element of the form) listens for any input from the user and count down the number as they input more characters
+  $('#tweet-text').on('input', function(event) {
     const lengthOfTextValue = $(this).val().length;
 
-    // the count is currently going up, to count down into the negatives, we have to flip the counter
-    // from 140, count down each time a character is input in the textarea
     const counter = 140 - lengthOfTextValue;
 
-    // get access to the sibling's child .counter by traversing up and down the DOM
-    // currently, you are on <textarea> element, there are two additional siblings <label> and <div>. We need to travel up to the parent <form> and back down to the grandchild <output>.
+    // Traverse up and down the DOM to get to '.counter'
     const counterChild = $(this).closest('.form').find('.counter');
-
-    // counterChild should count down as each character is input in the textarea
-    // .text replaces the value of that element
     counterChild.text(counter);
 
-    // if user exceeds the 140 character limit, the counter should appear red, otherwise, it should always be the other color
     if (counter < 0) {
       counterChild.css('color', '#B24926');
     } else {
